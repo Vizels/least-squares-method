5efffb0d9081ada61d9a925ced5fdfc96ea1f7d3
vizels <mirchuk.georgiy@gmail.com>
2023-12-02 23:33:57 +0100

    Object with parameters created

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 import measure
 
-test_wave = measure.Triangle_wave(3, 1000)
+test_wave = measure.Static_object(3, 1000)
+test_wave.set_parameters([1,1,1])
+test_wave.transform()
 
-
-plt.plot(test_wave.t, test_wave.original_y)
-plt.show()
+# plt.plot(test_wave.t, test_wave.u)
+# plt.show()
--- measure.py
+++ measure.py
@@ -2,8 +2,31 @@ import numpy as np
 from scipy import signal
 
 class Static_object:
-    def __init__(self, cycles, original_samples_number):
-        self.t = np.linspace(0, cycles*2*np.pi, original_samples_number)
-        self.original_y = signal.sawtooth(self.original_t, 0.5) 
+    def __init__(self, cycles, samples_number):
+        self.t = np.linspace(0, cycles*2*np.pi, samples_number)
+        self.u = np.random.rand(samples_number)
+
+    def set_parameters(self, parameters):
+        self.parameters = parameters
+
+    def transform(self):
+        self.v = np.zeros_like(self.u)
+
+        #set first two values manually, because u[k-1] and/or u[k-2] are not defined (equal zero)
+        self.v[0] = self.u[0] * self.parameters[0]
+        self.v[1] = self.u[1] * self.parameters[1] + self.u[0] * self.parameters[0]
+        
+        for i in range(2, len(self.u)):
+            #replace it with numpy dot function
+            self.v[i] = np.dot(self.parameters, self.u[i-2:i+1])
+        
+        print(self.v)
+            
+
+
+    
+        
 
         
+            
+            
