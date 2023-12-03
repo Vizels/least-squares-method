52b429a3bea1496619cc124154184b7126723d2c
vizels <mirchuk.georgiy@gmail.com>
2023-12-03 02:22:11 +0100

    Variable rename

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import measure
 
-test_wave = measure.Static_object(3, 1000)
-test_wave.set_parameters([1,1,1])
-test_wave.transform()
-test_wave.noise(0.1, -0.5)
+test_obj = measure.Static_object(3, 1000)
+test_obj.set_parameters([1,1,1])
+test_obj.transform()
+test_obj.noise(0.1, -0.5)
 
-plt.plot(test_wave.t, test_wave.y)
+plt.plot(test_obj.t, test_obj.y)
 plt.show()
